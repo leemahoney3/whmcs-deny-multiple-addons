d0f3c265eb2e999569bedab29eb17fdfa8f02faf
Lee Mahoney <lee@leemahoney.dev>
2022-10-07 12:17:19 +0100

    v102 update
    Added some extra functionality for logged in users and switched to Models.

--- includes/hooks/denyMultipleAddons.php
+++ includes/hooks/denyMultipleAddons.php
@@ -1,5 +1,6 @@
 <?php
 
+use WHMCS\Service\Addon;
 use WHMCS\Database\Capsule;
 
 /**
@@ -11,30 +12,27 @@ use WHMCS\Database\Capsule;
  * @author     Lee Mahoney <lee@leemahoney.dev>
  * @copyright  Copyright (c) Lee Mahoney 2022
  * @license    MIT License
- * @version    1.0.0
+ * @version    1.0.2
  * @link       https://leemahoney.dev
  */
-
-
 
 if (!defined('WHMCS')) {
     die('You cannot access this file directly.');
 }
 
-
 function deny_multiple_addons($vars) {
+
+    # Which statuses should be used to identify that the addon can no longer be added.
+    $statuses = [
+        'Active',
+        'Suspended',
+        'Pending',
+        'Completed',
+        'Fraud'
+    ];
 
     # Only run the following block of code on the "View Available Addons" page.
     if ($_SERVER['REQUEST_URI'] == '/cart.php?gid=addons') {
-
-        # Which statuses should be used to identify that the addon can no longer be added.
-        $statuses = [
-            'Active',
-            'Suspended',
-            'Pending',
-            'Completed',
-            'Fraud'
-        ];
 
         # Grab the addons currently being outputted on the page
         $theAddons  = $vars['addons'];
@@ -48,7 +46,7 @@ function deny_multiple_addons($vars) {
 
                 # Check the database for the current addon in question, that relates to the current user and relevant product (from the dropdown).
                 # Check also if the status is one of those listed in the $statuses array
-                $check = Capsule::table('tblhostingaddons')->where([
+                $check = Addon::where([
                     'hostingid' => $product['id'],
                     'addonid'   => $theAddon['id'], 
                     'userid'    => $_SESSION['uid']
@@ -88,9 +86,21 @@ function deny_multiple_addons($vars) {
     
     # Loop through the addons in the users session
     foreach ($_SESSION['cart']['addons'] as $key => $addon) {
-        
+
+        $userCheck = false;
+
+        # If the user is logged in and somehow the same addon got activated before they managed to complete the cart, add it for removal
+        if (isset($_SESSION['uid'])) {
+            $userCheck = Addon::where([
+                'hostingid' => $addon['productid'],
+                'addonid'        => $addon['id'], 
+                'userid'    => $_SESSION['uid']
+            ])->whereIn('status', $statuses)->count();
+        }
+
         # If the addon matches our array by id and product id, remove the addon's array from the users session (and redirect so it doesn't still show two in the cart until the user refreshes)
-        if($addonsArray[$addon['id']][$addon['productid']] == 1) {
+        # Same goes for if the user check above returns a result
+        if($addonsArray[$addon['id']][$addon['productid']] == 1 || $userCheck != 0) {
             unset($_SESSION['cart']['addons'][$key]);
             header("Location: {$_SERVER['REQUEST_URI']}");
         } else {
@@ -99,7 +109,6 @@ function deny_multiple_addons($vars) {
         }
 
     }
-
 
 }
 
